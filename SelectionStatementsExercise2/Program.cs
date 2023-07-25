f2468e89ce21606f682ba582fcddf52987bedc8d
Kelly Plyler <kellysplyler@gmail.com>
2023-07-25 16:43:46 -0500

    finished exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var favSubject = (Console.ReadLine());
+            
+            switch (favSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("What a nerd!");
+                    break;
+                case "english":
+                    Console.WriteLine("You must love reading!");
+                    break;
+                case "science":
+                    Console.WriteLine("Carl Sagan is my hero.");
+                    break;
+                case "gym":
+                    Console.WriteLine("You must be good at sports!");
+                    break;
+                default:
+                    Console.WriteLine("What a slacker!");
+                    break;
+
+            }
         }
     }
 }
